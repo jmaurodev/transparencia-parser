ab2fb53c6442c3c71601b18bb6e766a7698886d1
Jorge Mauro <jmaurodev@gmail.com>
2020-11-23 13:50:27 -0300

    Added parse and filter functions

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # transparencia-parser
 
-Need to set .env file with the following variables:
+### .env file
 
-1. URL="http://www.portaltransparencia.gov.br/download-de-dados/despesas/"
-2. EXPECTED_FORMAT=".zip"
+##### Need to set .env file with the following variables:
+
+1. URL
+2. EXPECTED_FORMAT
+3. UG_PRIMARIA
+4. UG_SECUNDARIA
+
+##### Sample
+
+```
+URL="http://www.portaltransparencia.gov.br/download-de-dados/despesas/"
+EXPECTED_FORMAT=".zip"
+UG_PRIMARIA="160296"
+UG_SECUNDARIA="167296"
+```
--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 import os
+import csv
+import re
 from zipfile import ZipFile
 import requests
 from dotenv import load_dotenv
@@ -10,26 +12,32 @@ def _load_env_vars(path=None):
     env_vars = {
         'NAME': os.getenv('NAME'),
         'URL': os.getenv('URL'),
-        'EXPECTED_FORMAT': os.getenv('EXPECTED_FORMAT')
+        'EXPECTED_FORMAT': os.getenv('EXPECTED_FORMAT'),
+        'UG_PRIMARIA': os.getenv('UG_PRIMARIA'),
+        'UG_SECUNDARIA': os.getenv('UG_SECUNDARIA'),
+        'DAY': os.getenv('DAY'),
+        'MONTH': os.getenv('MONTH'),
+        'YEAR': os.getenv('YEAR')
     }
     return env_vars
+
 
 def _prompt_user(values=None):
     values = {
         'day': input('Digite o dia: '),
         'month': input('Digite o mês: '),
-        'year': input('Digite o ano: '),
-        'ug': input('Digite a Unidade Gestora: ')
+        'year': input('Digite o ano: ')
     }
     return values
 
-def _download_file(url, date):   
+
+def _download_file(url, date):
     response = requests.get(
-        url+date, 
+        url+date,
         verify=False,
         stream=True
-        )
-    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    )
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
     block_size = 1024
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
     file_name = 'despesa' + date + env_vars['EXPECTED_FORMAT']
@@ -40,9 +48,48 @@ def _download_file(url, date):
             f.write(data)
         progress_bar.close()
 
+
 def _unzip_file(file_name):
     with ZipFile(file_name, 'r') as zip_ref:
         zip_ref.extractall()
+
+
+def _parse_empenho(date):
+    file_name = date + '_Despesas_Empenho'
+    ug = (
+        env_vars['UG_PRIMARIA'],
+        env_vars['UG_SECUNDARIA']
+    )
+    output = open(date + '_Output.csv', 'w', encoding='latin-1')
+    with open(file_name + '.csv', encoding='latin-1') as f:
+        reader = csv.reader(f, delimiter=';')
+        writer = csv.writer(output)
+        for line in reader:
+            if line[12] in ug:
+                writer.writerow((
+                    line[12],
+                    line[2],
+                    line[3],
+                    line[6],
+                    line[7],
+                    line[18],
+                    _filter('CODOM([0-9]+)', '[0-9]+', line[18]),
+                    _filter('RQ([0-9]+)', '[0-9]+', line[18]),
+                    _filter('NC([0-9]+)', '[0-9]+', line[18]),
+                    _filter('SRP([0-9]+/[0-9]{4}-[0-9]{6})',
+                            '[0-9]+/[0-9]{4}-[0-9]{6}', line[18]),
+                    _filter('OBS([\\w]+)', '\\w', line[18]),
+                    line[45]+line[47]+line[49]+line[51],
+                    line[60]
+                ))
+
+
+def _filter(shell, kernel, string):
+    result = re.search(shell, string)
+    if result:
+        return re.search(kernel, result)
+    else:
+        return None
 
 
 env_vars = _load_env_vars()
@@ -52,4 +99,5 @@ _download_file(env_vars['URL'], date)
 file_name = 'despesa' + date + env_vars['EXPECTED_FORMAT']
 if env_vars['EXPECTED_FORMAT'] == '.zip':
     _unzip_file(file_name)
+_parse_empenho(date)
 print('Success!')
